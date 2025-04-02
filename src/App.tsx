c7df5d4d41a0e02cbc1c569c87842787feb719f6
Артем Грушев <artemgrushev@gmail.com>
2025-04-03 00:09:35 +0500

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { Product } from "./components/product"
+import { products } from "./data/product"
+import axios, { AxiosError } from "axios";
+import { IProduct } from "./models";
+
+function App() {
+  const [products, setProducets] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProducts() {
+    try{
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products');
+      setProducets(response.data);
+      setLoading(false);
+    }
+    catch(e: unknown){
+      const error = e as AxiosError
+      setLoading(false);
+      setError(error.message)
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <p className="text center">Loading ...</p>}
+      {error && <p className="text center text-red-600">{error}</p>}
+      {products.map(product => <Product product={product} key={product.id}/>)}
+      {/* <Product product={products[0]}/>
+      <Product product={products[1]}/> */}
+    </div>
+  )
+}
+
+export default App
